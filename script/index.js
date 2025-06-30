f2a37ba1772dd53e3159123276bf57190c2ac620
AmedeoMignano <amedeo56@gmail.com>
2025-06-30 11:51:13 +0200

    Adds js

--- script/index.js
+++ script/index.js
@@ -7,17 +7,17 @@ window.addEventListener("scroll", function () {
 // classe "scrolled" se l'asse dello scroll della finestra è maggiore di 350px. con vari tentativi è il più vicino alla
 // fine dell'header
 
-const letters = document.querySelectorAll(".letters");
+const letters = document.querySelectorAll("svg g");
 
 const toggleRandomly = () => {
   letters.forEach((letter) => {
-    const delay = Math.random() * 2000;
+    const delay = Math.random() * 6000;
     setTimeout(() => {
       letter.classList.toggle("hidden");
     }, delay);
   });
 };
 
-setInterval(toggleRandomly, 1500);
+setInterval(toggleRandomly, 2000);
 // pensavo di targhettare in maniera corretta
 // ma sono solo riuscito a targhettare tutto l'svg non riesco a targhettare le singole m
